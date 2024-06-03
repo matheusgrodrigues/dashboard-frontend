9b22187345f16510c8940b0fc7e602b5193456e7
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-06-03 16:44:41 -0300

    Testando ideia edicao pagina

--- src/app/(cms)/dashboard/paginas/[slug]/page.tsx
+++ src/app/(cms)/dashboard/paginas/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Link from 'next/link';
 
 import { BaseLayoutContent } from '../../../../../app/components/BaseLayout';
@@ -6,21 +8,78 @@ import { getRoute } from '../../../../../core/utils/routes';
 import useRegisterDynamicBreadcrumbRoute from '@/core/utils/hooks/useRegisterDynamicBreadcrumbRoute';
 
 import breadcrumb from './breadcrumb';
+import { Stack } from '@mui/material';
 
 export const dynamicParams = false;
 
-export const generateStaticParams = () => {
-    const mockPages = [
-        {
-            id: '1',
-            nome: 'Home',
-            atividade: '31 de maio de 2024',
-            status: 'Online',
-        },
-    ];
+type PageSectionName = 'header-section';
+type ComponentName = 'heading-component';
+type PageName = 'home-page';
+type PageType = 'page';
 
-    return mockPages.map((page) => page);
+type SeoPageProps = {
+    description: string;
+    title: string;
 };
+
+type PageSection = {
+    displayName: string;
+    components: Component[];
+    isActive: boolean;
+    id: number;
+    type: PageSectionName;
+    name: string;
+};
+
+type Component = {
+    displayName: string;
+    isActive: boolean;
+    name: string;
+    type: ComponentName;
+    id: number;
+};
+
+type Page = {
+    displayName: string;
+    sections: PageSection[];
+    isActive: boolean;
+    name: PageName;
+    type: PageType;
+    seo: SeoPageProps;
+    id: number;
+};
+
+const fakePage: Page = {
+    displayName: 'Home Page',
+    isActive: true,
+    name: 'home-page',
+    type: 'page',
+    seo: {
+        description: 'Description',
+        title: 'Title',
+    },
+    id: 1,
+    sections: [
+        {
+            displayName: 'Header',
+            components: [
+                {
+                    displayName: 'Titulo Principal',
+                    isActive: true,
+                    name: 'titulo-principal',
+                    type: 'heading-component',
+                    id: 1,
+                },
+            ],
+            isActive: true,
+            name: 'header',
+            type: 'header-section',
+            id: 1,
+        },
+    ],
+};
+
+export const generateStaticParams = () => [fakePage].map((page) => page);
 
 interface PageProps {
     params: {
@@ -36,7 +95,25 @@ export default function Page({ params }: PageProps) {
 
     return (
         <BaseLayoutContent headerTitle={params.slug} breadcrumb={dynamicBreadcrumb.breadcrumb}>
-            {params.slug}
+            {fakePage.type === 'page' && (
+                <Stack>
+                    {fakePage.sections.map((section) => {
+                        return (
+                            <React.Fragment key={section.id}>
+                                <p>{section.displayName}</p>
+
+                                {section.components.map((component) => {
+                                    return (
+                                        <React.Fragment key={component.id}>
+                                            <p>{component.displayName}</p>
+                                        </React.Fragment>
+                                    );
+                                })}
+                            </React.Fragment>
+                        );
+                    })}
+                </Stack>
+            )}
 
             <Link href={`${getRoute('paginas').path}`}>Voltar</Link>
         </BaseLayoutContent>
