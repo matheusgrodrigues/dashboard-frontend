f599064ceabcbe39c71fcb7759000cdabbb1630d
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-06-17 13:06:25 -0300

    Validando mutations

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
+import { redirect } from 'next/navigation';
 import { FieldValues } from 'react-hook-form';
 
 import validateFormServerRules from '../core/utils/actions/action-rules';
+import { getRoute } from '../core/utils/routes';
 
 import formLoginRules from './rules';
 
@@ -13,7 +15,7 @@ export const loginClientAction = async (data: FieldValues) => {
 export const loginServerAction = async (prevState: unknown, queryData: FormData) => {
     const data = Object.fromEntries(queryData);
 
-    const validateRules = validateFormServerRules({ formDataEntryValue: data, rules: formLoginRules });
+    const validateRules = await validateFormServerRules({ formDataEntryValue: data, rules: formLoginRules });
 
-    return validateRules ?? data;
+    return validateRules ?? redirect(getRoute('paginas').path);
 };
--- src/app/rules.ts
+++ src/app/rules.ts
@@ -6,12 +6,6 @@ const formLoginRules = z.object({
             invalid_type_error: 'Email inválido',
         })
         .min(4),
-
-    password: z
-        .string({
-            invalid_type_error: 'Informe a senha',
-        })
-        .min(3),
 });
 
 export default formLoginRules;
